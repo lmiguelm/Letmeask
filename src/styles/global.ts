8769c0a4aaaeace1dd1afdbd4f4d5ed022358f76
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-06-23 00:46:51 -0300

    estilo no scroll

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,6 +16,18 @@ export const GlobalStyle = createGlobalStyle`
     font: 400 1rem 'Roboto', sans-serif;
   }
 
+  ::-webkit-scrollbar-track {
+    background: ${(props) => props.theme.gray.secondary};
+  }
+
+  ::-webkit-scrollbar {
+    width: 6px;
+    background: ${(props) => props.theme.gray.secondary};
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${(props) => props.theme.purple.normal};
+  }
+
   @media (max-width: 1080px) {
     html {
       font-size: 93.75%;
